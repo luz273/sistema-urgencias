cf320c4a9044f984d728d2df4b874b04ad47f0c6
LUZ <luzro@gmail.com>
2025-12-26 17:44:40 -0500

    con esto puedo ver admi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,31 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Hash; // <- IMPORTAR HASH
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
     public function run(): void
     {
-        // Crear SOLO el administrador único
-        User::firstOrCreate(
-            ['email' => 'admin@urgencias.com'],
-            [
-                'name' => 'Administrador',
-                'password' => Hash::make('12345678'),
-                'role' => 'administrador',
-            ]
-        );
+        // Crear usuario de prueba
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        // Crear administrador
+        User::create([
+            'name' => 'Admin MedAlert',
+            'email' => 'admin@urgencias.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'administrador',
+        ]);
     }
 }
